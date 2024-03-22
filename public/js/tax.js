261762072496ce2dea8a3cda1dd123c64d6c5673
sha <abc@gmail.com>
2024-03-22 08:45:16 +0530

    tax calculated

---
+++ public/js/tax.js
@@ -0,0 +1,22 @@
+let taxSwitch = document.querySelector(".check-tax");
+
+taxSwitch.addEventListener("click", ()=>{
+  let taxInfo = document.querySelectorAll(".tax-info");
+  let beforetaxInfo = document.querySelectorAll(".before-tax-info");
+    for(beforeinfo of beforetaxInfo){
+        if(beforeinfo.style.display != "none"){
+            beforeinfo.style.display ="none";
+        }
+        else{
+            beforeinfo.style.display ="contents";
+        }
+      }
+    for(info of taxInfo){
+        if(info.style.display != "contents"){
+            info.style.display ="contents";
+        }
+        else{
+            info.style.display ="none";
+        }
+  }
+})
